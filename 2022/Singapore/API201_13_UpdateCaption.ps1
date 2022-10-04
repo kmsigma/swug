e1b1c792a46849c5982afa31f3308e2f8e2fb56b
kmsigma <kevin.sparenberg@solarwinds.com>
2022-10-04 16:49:51 +0800

    typo

--- 2022/Singapore/API201_13_UpdateCaption.ps1
+++ 2022/Singapore/API201_13_UpdateCaption.ps1
@@ -1,6 +1,6 @@
 <#
 
-Finds a node with 'WESTIIS01V' as the caption and updates it to 'WESTWEB01v'
+Finds a node with 'westiis01v.demo.lab' as the caption and updates it to 'WESTWEB01v'
 
 #>
 if ( -not $SwisConnection ) {
@@ -8,12 +8,12 @@ if ( -not $SwisConnection ) {
     $SwisConnection = Connect-Swis -Hostname $Hostname -Credential ( Get-Credential -Message "Enter your Orion credentials for $Hostname" )
 }
 
-Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Uri FROM Orion.Nodes WHERE Caption = 'WESTIIS01V'"
+Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Uri FROM Orion.Nodes WHERE Caption = 'westiis01v.demo.lab'"
 
-$Uri = Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Uri FROM Orion.Nodes WHERE Caption = 'WESTIIS01V'"
+$Uri = Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Uri FROM Orion.Nodes WHERE Caption = 'westiis01v.demo.lab'"
 
 $Properties = @{
-    Caption = 'WESTWEB01v'
+    Caption = 'westweb01v.demo.lab'
 }
 
 Set-SwisObject -SwisConnection $SwisConnection -Uri $Uri -Properties $Properties
