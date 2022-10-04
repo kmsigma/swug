63d981ed0201bc9a3170959cd3abc25462e4ac87
kmsigma <kevin.sparenberg@solarwinds.com>
2022-10-04 09:42:32 +0800

    Singapore Files

---
+++ 2022/Singapore/API201_30_SuppressLOSA.ps1
@@ -0,0 +1,24 @@
+<#
+
+Suppress alerts for all LOSA devices
+
+#>
+if ( -not $SwisConnection ) {
+    $Hostname = "10.196.3.11"
+    $SwisConnection = Connect-Swis -Hostname $Hostname -Credential ( Get-Credential -Message "Enter your Orion credentials for $Hostname" )
+}
+
+
+$NodesToMute = Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Caption, Uri FROM Orion.Nodes WHERE IPAddress LIKE '10.149.%.%'"
+
+# We are in the central time zone, right now, so those are reflected in the hand-typed entries below
+$suppressFrom  = Get-Date "2022-09-17 01:00 AM" # 11 PM, Friday - Los Angeles Time
+$suppressUntil = Get-Date "2022-09-18 04:00 PM" # 5 PM, Sunday  - Los Angeles Time
+# Flip them to UTC
+$suppressFrom = $suppressFrom.ToUniversalTime()
+$suppressUntil = $suppressUntil.ToUniversalTime()
+
+Invoke-SwisVerb -SwisConnection $SwisConnection -EntityName 'Orion.AlertSuppression' -Verb 'SuppressAlerts' -Arguments $NodesToMute.Uri , $suppressFrom, $suppressUntil
+
+
+
---
+++ 2022/Singapore/Reset.ps1
@@ -0,0 +1,83 @@
+<#
+
+Reset all changes from SWUG scripts
+Most Recent Updates: 02-AUG-2022
+
+#>
+if ( -not $SwisConnection ) {
+    $Hostname = "10.196.3.11"
+    $SwisConnection = Connect-Swis -Hostname $Hostname -Credential ( Get-Credential -Message "Enter your Orion credentials for $Hostname" )
+}
+
+
+$Uri = Get-SwisData -SwisConnection $SwisConnection -Query "SELECT Uri FROM Orion.Nodes WHERE NodeID = 154 AND Caption <> 'WESTIIS01V.demo.lab'"
+
+$Properties = @{
+    Caption = 'westiis01v.demo.lab'
+}
+
+if ( $Uri ) {
+    Write-Host "Updating Caption on Node 154 to 'WESTIIS01V'" -NoNewline
+    Set-SwisObject -SwisConnection $SwisConnection -Uri $Uri -Properties $Properties
+    Write-Host " [COMPLETED]" -ForegroundColor Green
+}
+else {
+    Write-Host "Don't need to update the Caption on Node 154" -ForegroundColor Yellow
+}
+
+$ResetCaptionDomainCP = @"
+SELECT [Node].Caption
+     , [Node].Uri AS [NodeUri]
+     , [Node].CustomProperties.DomainName
+     , [Node].CustomProperties.Uri AS [CPUri]
+FROM Orion.Nodes AS [Node]
+WHERE (
+     IsNull([Node].CustomProperties.DomainName, '') <> ''
+  OR [Node].Caption NOT LIKE '%.demo.lab'
+  )
+ AND [Node].ObjectSubType <> 'ICMP'
+
+ORDER BY [Node].Caption
+"@
+
+$NodesToReset = Get-SwisData -SwisConnection $SwisConnection -Query $ResetCaptionDomainCP
+
+ForEach ( $ResetNode in $NodesToReset ) {
+    $NewCaption = ( $ResetNode.Caption.Split('.')[0] ).ToLower() + '.demo.lab'
+    Write-Host "Updating $( $ResetNode.Caption ) to $NewCaption" -NoNewline
+    Set-SwisObject -SwisConnection $SwisConnection -Uri $ResetNode.NodeUri -Properties @{
+        'Caption' = $NewCaption
+    }
+    Set-SwisObject -SwisConnection $SwisConnection -Uri $ResetNode.CPUri -Properties @{
+        'DomainName' = $null
+    }
+    Write-Host " [COMPLETED]" -ForegroundColor Green
+}
+
+# Resetting Muted Devices
+
+$MutedThingsQuery = @"
+SELECT EntityUri AS [Uri]
+     , SuppressFrom
+     , SuppressUntil
+FROM Orion.AlertSuppression
+"@
+
+$MutedThings = Get-SwisData -SwisConnection $SwisConnection -Query $MutedThingsQuery
+<#
+$EntityXml = '<ArrayOfstring xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">'
+$MutedThings | ForEach-Object { $EntityXml += "<string>$( $_.Uri )</string>" }
+$EntityXml += '</ArrayOfstring>'
+$EntityXml
+#>
+
+Write-Host "Unmuting $( $MutedThings.Count) entities"
+ForEach ( $Uri in $MutedThings.Uri ) {
+    Invoke-SwisVerb -SwisConnection $SwisConnection -EntityName 'Orion.AlertSuppression' -Verb 'ResumeAlerts' -Arguments @(, $Uri)
+}
+Write-Host " [COMPLETED]" -ForegroundColor Green
+
+
+For ( $i = 0; $i -lt $MutedThings.Count; $i++ ) {
+    Invoke-SwisVerb -SwisConnection $SwisConnection -EntityName 'Orion.AlertSuppression' -Verb 'GetAlertSuppressionState' -Arguments ( @(, $MutedThings[$i].Uri ))
+}
