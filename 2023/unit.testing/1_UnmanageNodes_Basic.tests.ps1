b9a8f51e6bb33c6fdbc757707493b0633ee7fd34
kmsigma <kevin.sparenberg@kmsigma.com>
2023-05-24 09:20:21 -0400

    Formatting Fixes

--- 2023/unit.testing/1_UnmanageNodes_Basic.tests.ps1
+++ 2023/unit.testing/1_UnmanageNodes_Basic.tests.ps1
@@ -14,41 +14,43 @@ if ( -not $SwisConnection ) {
 #endregion setup
 
 Describe  'Get-SwisNodeIds' {
-    Context "When called with default test system"{
-        it "runs without error"{
-            {Get-SwisNodeIds($SwisConnection)} | Should -Not -Throw
+    Context "When called with default test system" {
+        it "runs without error" {
+            {
+                Get-SwisNodeIds($SwisConnection)
+            } | Should -Not -Throw
         }
-        it "returns 4 nodeids"{
+        it "returns 19 nodeids" {
             $results = Get-SwisNodeIds($SwisConnection)
             $results.Count | should -be 19
         }
     }
-    Context "When called no connection"{
-        it "runs should throw an error"{
-            {Get-SwisNodeIds($null)} | should  -throw
+    Context "When called no connection" {
+        it "runs should throw an error" {
+            {
+                Get-SwisNodeIds($null)
+            } | should -throw
         }
     }
 }
    
 Describe  'Set-SwisNodesUnmanagedById' {
-  BeforeAll{
-            [int32[]]$ids = '1010','1012','1013','1022'
-        }
-    Context "When called with clean parameters"{
-      
-        it "runs without error for a single id"{
-            {Set-SwisNodesUnmanagedById -swis $SwisConnection -ids 1022} | should -Not -throw
-        }
-
-        it "runs without error for multiple ids"{
-            {Set-SwisNodesUnmanagedById -swis $SwisConnection -ids $ids} | should -Not -throw
-        }
-       
-
-        
+    BeforeAll {
+        [int32[]]$ids = '1010', '1012', '1013', '1022'
     }
+    Context "When called with clean parameters" {
+      
+        it "runs without error for a single id" {
+            {
+                Set-SwisNodesUnmanagedById -swis $SwisConnection -ids 1022
+            } | should -Not -throw
+        }
 
-
-
+        it "runs without error for multiple ids" {
+            {
+                Set-SwisNodesUnmanagedById -swis $SwisConnection -ids $ids
+            } | should -Not -throw
+        }
+    }
 }
 
